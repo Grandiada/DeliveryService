b5aefd95b9d179ff13edc62d955907fbabb67b92
Grandiada <shchepko@gmail.com>
2020-10-08 01:48:24 +0300

    Boilerplate commit

---
+++ src/app/sagas.ts
@@ -0,0 +1,12 @@
+import { all } from "@redux-saga/core/effects";
+import { minesweaperSagas } from "../features/Minesweaper/duck";
+
+function SETUP() {
+  console.log("App started");
+}
+
+export default function* rootSaga() {
+  yield all([
+    SETUP(),
+  ]);
+}
